3ffd12b49bcfb5b2c82100856c4a983c088eeb28
Samuel Ortmans <samuel.ortmans@gmail.com>
2020-01-22 15:51:22 +0200

    sqlite db tables created

--- backend/tables.py
+++ backend/tables.py
@@ -1,22 +1,22 @@
 tables =   ["""
             CREATE TABLE IF NOT EXISTS Incidents (
-                id integer PRIMARY KEY,
+                id integer PRIMARY KEY AUTOINCREMENT,
                 lat float NOT NULL,
                 lon float NOT NULL,
                 category text,
                 start_time datetime default current_timestamp,
-                end_time datetime default current_timestamp
+                end_time datetime default NULL
             );
             """,
             """
             CREATE TABLE IF NOT EXISTS Media (
-                id integer PRIMARY KEY,
+                id integer PRIMARY KEY AUTOINCREMENT,
                 path text NOT NULL
             );
             """,
             """
             CREATE TABLE IF NOT EXISTS Users (
-                id integer PRIMARY KEY,
+                id integer PRIMARY KEY AUTOINCREMENT,
                 phone_number text UNIQUE NOT NULL
             );
             """
