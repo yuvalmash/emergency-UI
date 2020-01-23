6ae2f4482738884bade7781be2bf678580fabfbf
Raz <razzo.des@gmail.com>
2020-01-23 03:51:10 +0200

    add: wind speed from weather API

--- static/user/js/post.js
+++ static/user/js/post.js
@@ -1,15 +1,24 @@
-    const user_response = {};
+const user_response = {};
 
 const geo = navigator.geolocation;
 
 if (navigator.geolocation) {
   geo.getCurrentPosition(logPosition);
 }
+function getWindData(lat, lon) {
+  fetch(
+    `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=abbbb0aeec52a169752b89b579f7f765`
+  )
+    .then(res => res.json())
+    .then(body => (user_response.windSpeed = body["wind"]["speed"]));
+  // .then(data => console.log(data));
+}
 
 function logPosition(position) {
   user_response.lat = position.coords.latitude;
   user_response.lon = position.coords.longitude;
-  console.log(position.coords);
+  getWindData(user_response.lat, user_response.lon);
+  // console.log(position.coords);
   console.log(">>>>", user_response);
 }
 
@@ -65,7 +74,7 @@ addPhoto.addEventListener("click", function() {
   ctx.drawImage(videoElm, 0, 0, captureCanvas.width, captureCanvas.height);
   img.src = captureCanvas.toDataURL("image/png");
   img.width = 240;
-//  console.log('XXXXXXXXXX',img.src);
+  //  console.log('XXXXXXXXXX',img.src);
   user_response.img = img.src;
   captureCanvas.appendChild(img);
 });
@@ -74,12 +83,12 @@ const postToServer = document.querySelector("#postToServer");
 
 postToServer.addEventListener("click", function() {
   console.log(user_response);
-  fetch('/user_response/',
-  {
-    method: 'POST',
+  fetch("/user_response/", {
+    method: "POST",
     headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify(user_response)});
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(user_response)
+  });
 });
